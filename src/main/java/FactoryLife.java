b41ca04230450f3aa7ef174bab1670aeb5083fe0
Pengyi Zhang <edenthink@gmail.com>
2015-12-04 22:46:12 +1100

    initial

---
+++ src/main/java/FactoryLife.java
@@ -0,0 +1,18 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class
+ */
+public class FactoryLife extends JavaPlugin {
+
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
