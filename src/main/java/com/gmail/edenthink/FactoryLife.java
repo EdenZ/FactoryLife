bf6c61752ea62703355d1a0ffd9396f42d30c253
Pengyi Zhang <edenthink@gmail.com>
2015-12-04 22:56:55 +1100

    Driver

--- src/main/java/com/gmail/edenthink/FactoryLife.java
+++ src/main/java/com/gmail/edenthink/FactoryLife.java
@@ -1,20 +1,59 @@
 package com.gmail.edenthink;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 /**
  * Main class
  */
 public class FactoryLife extends JavaPlugin {
-
+    private static final Logger log = Logger.getLogger("Minecraft");
+    public static Economy econ = null;
+    public static Permission perms = null;
+    public static Chat chat = null;
 
     @Override
     public void onDisable() {
-        super.onDisable();
+        log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
     }
 
     @Override
     public void onEnable() {
-        super.onEnable();
+        if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        setupPermissions();
+        setupChat();
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
     }
 }
---
+++ src/main/java/com/gmail/edenthink/util/Driver.java
@@ -0,0 +1,54 @@
+package com.gmail.edenthink.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Database driver
+ */
+public class Driver {
+    private static Connection connection = null;
+    private static final String DATABASE_URI = "jdbc:sqlite:./plugins/FactoryLife/storage.db";
+
+    /**
+     * Connect to database
+     */
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(DATABASE_URI);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Get the connection
+     * @return database connection
+     */
+    public static Connection getConnection() {
+        if (connection == null) {
+            connect();
+        }
+        return connection;
+    }
+
+    /**
+     * Disconnect
+     */
+    public static void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                System.err.println("Cannot close connection");
+            }
+        }
+    }
+}
