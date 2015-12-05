7bce61b78a7781b533d7e2cd7b4433354c7169c3
Pengyi Zhang <edenthink@gmail.com>
2015-12-06 02:14:14 +1100

    process order

--- src/main/java/com/gmail/edenthink/order/Controller.java
+++ src/main/java/com/gmail/edenthink/order/Controller.java
@@ -1,8 +1,10 @@
 package com.gmail.edenthink.order;
 
 import com.gmail.edenthink.FactoryLife;
+import com.gmail.edenthink.tools.Util;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * The controlling class for order system
@@ -32,8 +34,31 @@ public class Controller implements Listener{
         manager.resetOrderTimes();
     }
 
+    @SuppressWarnings("deprecation")
     public void processOrder(Player player, int orderNo) {
         // TODO: 2015/12/5  if user success to complete the order, reward user, else tell user failed
+        if (orderNo == 1) {
+            processStep(player, manager.ORDER_ONE, new ItemStack[]{new ItemStack(42, 5), new ItemStack(190, 5), new ItemStack(190, 5, (short) 1)}, 70);
+        } else if (orderNo == 2) {
+            processStep(player, manager.ORDER_TWO, new ItemStack[]{new ItemStack(367, 40), new ItemStack(352, 40), new ItemStack(289, 30)}, 50);
+        } else if (orderNo == 3) {
+            processStep(player, manager.ORDER_THREE, new ItemStack[]{new ItemStack(264, 5), new ItemStack(368, 8)}, 130);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void processStep(Player player, String orderType, ItemStack[] items, double reward) {
+        for (ItemStack item : items) {
+            if (!Util.checkItem(player.getInventory(), item.getTypeId(), item.getDurability(), item.getAmount())) {
+                return;
+            }
+        }
+        if (manager.orderTimesReduce(player.getName(), orderType)) {
+            for (ItemStack item : items) {
+                Util.removeItems(player.getInventory(), item.getTypeId(), item.getDurability(), item.getAmount());
+            }
+            FactoryLife.getEcon().depositPlayer(player, reward);
+        }
     }
 
     public void joinPlayerOrderCheck(Player player) {
