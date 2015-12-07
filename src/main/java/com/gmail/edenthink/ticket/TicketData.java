1ee5e6c359ed152303cae9fb83715cc4a7ca9d4c
Pengyi Zhang <edenthink@gmail.com>
2015-12-07 19:13:13 +1100

    ticket

---
+++ src/main/java/com/gmail/edenthink/ticket/TicketData.java
@@ -0,0 +1,23 @@
+package com.gmail.edenthink.ticket;
+
+/**
+ * Created by Eden on 2015/12/7.
+ */
+public class TicketData {
+    public final String TABLE = "player_ticket";
+    public final String PLAYER = "player";
+    public final String TICKET = "ticket";
+
+    public void newPlayer(String player) {
+        // TODO: 2015/12/7
+    }
+
+    public int getTicket(String player) {
+        // TODO: 2015/12/7
+        return 0;
+    }
+
+    public void setTicket(String player, int amount) {
+        // TODO: 2015/12/7
+    }
+}
