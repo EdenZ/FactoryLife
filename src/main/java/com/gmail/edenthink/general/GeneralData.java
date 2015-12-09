c4858d97773b3da7935aafc51ea812b48f5b1340
Pengyi Zhang <edenthink@gmail.com>
2015-12-09 20:24:57 +1100

    data

--- src/main/java/com/gmail/edenthink/general/GeneralData.java
+++ src/main/java/com/gmail/edenthink/general/GeneralData.java
@@ -2,6 +2,7 @@ package com.gmail.edenthink.general;
 
 import com.gmail.edenthink.tools.Driver;
 import com.gmail.edenthink.tools.Util;
+import org.bukkit.Location;
 
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -30,6 +31,28 @@ public class GeneralData {
 
     public static void setRemain(String player, int num) {
 
+    }
+
+    public static Location getChestLocation(String player) {
+        Location loc = null;
+        return loc;
+    }
+
+    public static void setChestLocation(String player, double x, double y, double z, String world) {
+        String sql = String.format("INSERT INTO location " +
+                "VALUES (%s, \'respawnchest\', %d, %d, %d, %s);", player, x, y, z, world);
+        if (getChestLocation(player) != null) {
+            sql = String.format("UPDATE location" +
+                    "SET x = %d, y = %d, z = %d, world = \"%s\"" +
+                    "WHERE owner = \"%s\" AND type = \"respawnchest\";", x, y, z, world, player);
+        }
+        try (Statement statement = Driver.getConnection().createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            if (e.getErrorCode() != 0) {
+                Util.printSQLError(e);
+            }
+        }
     }
 
     public static void resetRemain() {
